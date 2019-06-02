ce683c29df935ced9221df219353175b177b29f7
Lorin Vacarasu <juv338@gmail.com>
2019-06-02 22:24:39 +0300

    schimbari duminica seara

--- Exercitii_Munca/Program.cs
+++ Exercitii_Munca/Program.cs
@@ -32,6 +32,86 @@ namespace Exercitii_Munca
 			rezolvare_Exercitii.Oglindit();
 			Console.WriteLine();
 
+			//Console.WriteLine("EX 6.");
+			//rezolvare_Exercitii.AnBisect();
+			//Console.WriteLine();
+
+			Console.WriteLine("EX 7.");
+			rezolvare_Exercitii.Duminca();
+			Console.WriteLine();
+
+			//Console.WriteLine("EX 8.");
+			//rezolvare_Exercitii.convertorTemperatura();
+			//Console.WriteLine();
+
+			Console.WriteLine("EX 9.");
+			rezolvare_Exercitii.Extension_of_filename();
+			Console.WriteLine();
+
+			Console.WriteLine("EX 10.");
+			rezolvare_Exercitii.Diferenta(10);
+			rezolvare_Exercitii.Diferenta(20);
+			Console.WriteLine();
+
+			Console.WriteLine("EX 11.");
+			rezolvare_Exercitii.Suma(25, 18);
+			rezolvare_Exercitii.Suma(25, 125);
+			rezolvare_Exercitii.Suma(42, 42);
+			rezolvare_Exercitii.Suma(1001, 21321);
+			rezolvare_Exercitii.Suma(233432, 32323);
+			Console.WriteLine();
+
+			Console.WriteLine("EX 12.");
+			rezolvare_Exercitii.Verificare(1);
+			rezolvare_Exercitii.Verificare(20);
+			rezolvare_Exercitii.Verificare(50);
+			rezolvare_Exercitii.Verificare(100);
+			rezolvare_Exercitii.Verificare(400);
+			rezolvare_Exercitii.Verificare(341);
+			rezolvare_Exercitii.Verificare(500);
+			Console.WriteLine();
+
+			Console.WriteLine("EX 13.");
+			rezolvare_Exercitii.Pozitivitate(-25, 18);
+			rezolvare_Exercitii.Pozitivitate(25, -32);
+			rezolvare_Exercitii.Pozitivitate(42, 32);
+			rezolvare_Exercitii.Pozitivitate(-42, -23);
+			Console.WriteLine();
+
+			Console.WriteLine("EX 14.");
+			rezolvare_Exercitii.Pycaractere("Python");
+			rezolvare_Exercitii.Pycaractere("Incinta");
+			rezolvare_Exercitii.Pycaractere("asdadasPythonsadasdsa");
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 15.");
+			rezolvare_Exercitii.StergeCaracterul("removeoneofus");
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 16.");
+			rezolvare_Exercitii.AdaugaCapte("liverpool");
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 17.");
+			rezolvare_Exercitii.Multiplu_treisapte(211);
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 18.");
+			rezolvare_Exercitii.Javaverificare("Javasadasdsad");
+			rezolvare_Exercitii.Javaverificare("abcJavadef");
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 19.");
+			Console.WriteLine(rezolvare_Exercitii.Script("Javascriptttt"));
+			Console.WriteLine(rezolvare_Exercitii.Script("Javaascriptttt"));
+			Console.WriteLine(rezolvare_Exercitii.Script("asds"));
+			Console.WriteLine();
+
+			Console.WriteLine("Ex 20.");
+			Console.WriteLine(rezolvare_Exercitii.Caracteremici("as"));
+			Console.WriteLine(rezolvare_Exercitii.Caracteremici("ABCliverpool"));
+			Console.WriteLine();
+
 
 
 
--- Exercitii_Munca/Rezolvare exercitii.cs
+++ Exercitii_Munca/Rezolvare exercitii.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;                                                 //"HH:mm:ss tt"
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Exercitii_Munca
 {
 	class Rezolvare_exercitii
 	{
+
 		//// 1. Write a   function to display the current day and time in the following format.
 		//      ◦ Sample Output : 
 		//                  ◦ Today is : Tuesday.
@@ -40,7 +42,7 @@ namespace Exercitii_Munca
 			Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy"));
 			Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy"));
 
-			// Daca folosesc mm imi va afisa minutele in loc de luna curenta
+			// Daca folosesc "mm" imi va afisa minutele in loc de luna curenta
 
 		}
 
@@ -55,7 +57,7 @@ namespace Exercitii_Munca
 			int semipermitru = (a + b + c) / 2;
 
 			double suprafata = Math.Sqrt(semipermitru * (semipermitru - a) * (semipermitru - b) * (semipermitru - c));
-			Console.WriteLine("Aria triunghiului este {0}", suprafata );
+			Console.WriteLine("Aria triunghiului este {0}", suprafata);
 		}
 
 		// 5. Write a function to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front
@@ -71,5 +73,239 @@ namespace Exercitii_Munca
 			}
 			Console.WriteLine(oglindit);
 		}
+
+		// 6. Write a function to determine whether a given year is a leap year in the Gregorian calendar
+
+		public void AnBisect()
+		{
+			int An;
+			Console.Write("Introduceti anul : ");
+			An = Convert.ToInt32(Console.ReadLine());
+			if ((An % 4 == 0 && An % 100 != 0) || (An % 400 == 0))
+			{
+				Console.WriteLine(An + " este an bisect ");
+			}
+			else
+			{
+				Console.WriteLine(An + " nu este an bisect");
+			}
+			Console.ReadLine();
+		}
+
+		internal void JavaVerificare(string v)
+		{
+			throw new NotImplementedException();
+		}
+
+		// 7. Write a  function to find 1st January is being a Sunday between 2014 and 2050
+
+		public void Duminca()
+		{
+			for (int i = 2014; i <= 2050; i++)
+			{
+				DateTime datacautata = new DateTime(i, 1, 1);
+				if (datacautata.DayOfWeek == DayOfWeek.Sunday)
+				{
+					Console.WriteLine(datacautata.ToShortDateString());
+				}
+			}
+		}
+
+
+		// 8.Write a function to convert temperatures to and from Celsius, Fahrenheit.
+		//◦ Expected Output : 
+		//    1. 60°C is 140 °F
+		//    2. 45°F is 7.222222222222222°C
+
+		public void convertorTemperatura()
+		{
+			double f, c;
+			Console.WriteLine("Introduceti temperatura in grade Celsius:");
+			c = Convert.ToDouble(Console.ReadLine());
+			f = c * 9 / 5 + 32;
+			Console.WriteLine(c + "°C in Fahrenheit este: " + f + "°F");
+			Console.WriteLine("Introduceti temperatura in grade Fahrenheit:");
+			f = Convert.ToDouble(Console.ReadLine());
+			c = (f - 32) * 5 / 9;
+			Console.WriteLine(f + "°F in Celsius is: " + c + "°C");
+			Console.ReadLine();
+		}
+
+		// 9. Write a exercise to get the extension of a filename. (5 inputs)
+
+		public void Extension_of_filename()
+		{
+			string fileName = @"C:\Users\Lorin\Desktop\Documentatie Lorin Vacarasu.pdf";
+			FileInfo fisier = new FileInfo(fileName);
+			Console.WriteLine("File Extension: {0}", fisier.Extension);
+
+
+		}
+
+		// 10. Write a function to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference. ( example with 5 inputs)
+
+		public void Diferenta(int n)
+		{
+			if (n <= 13)
+			{
+				Console.WriteLine(13 - n);
+			}
+			else
+			{
+				Console.WriteLine((13 - n) * 2);
+			}
+
+		}
+
+		// 11. Write a function to compute the sum of the two given integers. If the two values are same, then returns triple their sum. (5 inputs)
+
+		public void Suma(int a, int b)
+		{
+			if (a > b || a < b)
+			{
+				Console.WriteLine(a + b);
+			}
+			else if (a == b)
+			{
+				Console.WriteLine(3 * (a + b));
+
+			}
+		}
+
+		// 12. Write a function to check a given integer is within 20 of 100 or 400. (5 inputs)
+
+		public void Verificare(int n)
+		{
+			if ((n > 20 && n < 100) || (n > 20 && n < 400))
+			{
+				Console.WriteLine(n + " se afla intre intervalul specificat");
+			}
+			else
+			{
+				Console.WriteLine(n + " nu se afla in intervalul specificat");
+			}
+		}
+
+		// 13. Write a   function to check from two given integers, if one is positive and one is negative.
+
+		public void Pozitivitate(int a, int b)
+		{
+
+			if (a < 0 && b > 0)
+			{
+				Console.WriteLine(a + " este numar negativ" + " si " + b + " este numar pozitiv");
+			}
+			else if (a > 0 && b < 0)
+			{
+				Console.WriteLine(b + " este numar negativ" + " si " + a + " este numar pozitiv");
+			}
+			else if (a > 0 && b > 0)
+			{
+				Console.WriteLine("Ambele numere sunt pozitive");
+			}
+			else if (a < 0 && b < 0)
+			{
+				Console.WriteLine("Ambele numere sunt negative");
+			}
+
+
+		}
+
+		// 14.Write a function to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string. (3 inputs/runs)
+
+		public void Pycaractere(string s)
+		{
+			string ss = s.Substring(0, 2);
+
+			if (ss == "Py")
+			{
+				Console.WriteLine(s);
+			}
+			else
+			{
+				Console.WriteLine("Py" + s);
+			}
+		}
+
+		// 15. Write a function to remove a character at the specified position of a given string and return the new string.
+
+		public void StergeCaracterul(string s)
+		{
+			Console.WriteLine(s.Remove(5, 1));
+
+		}
+
+		// 16. Write a function to create a new string from a given string with the first character of the given string added at the front and back.
+
+		public void AdaugaCapte(string s)
+		{
+			string primul = s.Substring(0, 1);
+			Console.WriteLine(primul + s + primul);
+		}
+
+		// 17. Write a function check if a given positive number is a multiple of 3 or a multiple of 7.
+
+		public void Multiplu_treisapte(int a)
+		{
+			Console.WriteLine(a % 3 == 0 || a % 7 == 0 ? true : false);
+		}
+
+		// 18. Write a   function to check if a string starts with 'Java' and false otherwise.
+
+		public void Javaverificare(string s)
+		{
+			if (s.Length < 4)
+			{
+				Console.WriteLine(false); ;
+			}
+
+			string ss = s.Substring(0, 4);
+
+			if (ss == "Java")
+			{
+				Console.WriteLine(true);
+			}
+			else
+			{
+				Console.WriteLine(false);
+			}
+		}
+
+		// 19. Write a function to check if a string "Script" presents at 5th (index 4) position in a given string, if "Script" presents in the string return the string without "Script" otherwise return the original one
+
+		public string Script (string s)
+		{	
+			if (s.Length < 6)
+			{
+				return s;
+			}
+
+			else if (s.Substring(4,6).Equals("script"))
+			{
+				return s.Replace("script", "");
+			}
+			else
+			{
+				return s;
+			}
+		}
+
+		// 20. Write a function to create new string with first 3 characters are in lower case. If the string length is less than 3 convert all the characters in upper case
+
+		public string Caracteremici(string s)
+		{
+			if (s.Length < 3)
+			{
+				return s.ToUpper();
+			}
+
+			string ss = s.Substring(0, 3);
+
+			return ss.ToLower() + s.Substring(3,s.Length -3);
+
+		
+		} 
+
+
 	}
 }
